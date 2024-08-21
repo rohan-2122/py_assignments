335d4bbd2224482cf6c5f46c08568679c99044c6
Rohan Chandele <124363185+rohan-2122@users.noreply.github.com>
2024-08-21 11:46:19 +0530

    Update q7.py

--- q7.py
+++ q7.py
@@ -2,24 +2,20 @@
 
 import random
 
-def play():
-    user = input("'r' for Rock, 'p' for Paper, 's' for Scissors: ")
-    computer = random.choice(['r','p','s'])
-    
-    if user == computer:
-             print('Its a tie...')
+user = input("'r' for Rock, 'p' for Paper, 's' for Scissors: ")
+computer = random.choice(['r','p','s'])
 
-    if is_win(user, computer):
-            print('You Win!!!')
-    def is_win(player, opponent):
+if user == computer:
+         print('Its a tie...')
+else:
+
+    def is_Win(player, opponent):
         # This function will return true if the player wins...
         # He will win if r>s, s>p, p>r
         if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         
             return True
-
-    print('You Lose !!!')
-
-
-play()
-    
+    if is_Win(user,computer):
+            print('You Win!!!')
+    else:
+            print('You Lose !!!')
